38cbf84fd4c9595ad665089556dbadd906f3a48d
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2022-02-24 15:18:31 +0000

    reviser les fondementaux de php

---
+++ php-fondementaux.php
@@ -0,0 +1,21 @@
+<style>
+    h2{
+        border-top:1px solid navy;
+        border-bottom: 1px solid navy;
+        color:navy;
+    }
+    .bu{
+        color:blue;
+    }
+    .bc{
+        color:grey;
+    }
+    .red{
+        color:red;
+    }
+    
+    table, td{
+        border: 1px solid green;
+        border-collapse : collapse;
+    }
+</style>
