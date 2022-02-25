084d273c171914999a8d4d9a8018993c9207c076
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2022-02-25 01:05:07 +0000

    notion de constante et les opérations arithmetiques

--- php-fondementaux.php
+++ php-fondementaux.php
@@ -4,13 +4,13 @@
         border-bottom:1px solid navy;
         color:navy;
     }
-    .bu{
-        color:blue;
+    .vert{
+        color:green;
     }
-    .bc{
-        color:grey;
+    .jaune{
+        color:yellow;
     }
-    .red{
+    .rouge{
         color:red;
     }
     
@@ -86,5 +86,72 @@ $a = "Hello ";
 $b = "everybody";
 echo $a . $b . '<br/>'; //On concatène les deux variables  et un string.Le point de concaténation peut être traduit par "suivi de.".
 
+#-----------------------------------------------------------------
+echo "<hr> Concaténation et aaffectation combinées avec .= <br>";
+$messageError = "Erreur sur le pseudo <br>";
+$messageError .= "Erreur sur le mot de passe <br>";
+print $messageError;
 
+$message = "aujourd'hui" ; // ou alors :
+$message = 'aujourd\'hui' ; // on échappe l'apostrophe écrite dans des quotes simples.
 
+$txt = 'Bonjour';
+echo "$txt tout le monde <br>"; // Dans les guillemets c'est son contenu qui est affiché.
+echo '$txt tout le monde <br>'; // Dans des quotes simples, tout est du texte brut.on voit donc le nom de la variable en toute lettre.
+
+// -------------------------------
+echo '<h2> Constante  </h2>';
+// -------------------------------
+/* Une constante est comme une variable qui permet de conserver une valeur, mais celle ci ne peut être modifié durant l'exécution du script.
+Exemple d'utilisation: on met les paramêtres de la BDD(base de données) pour ne pas risquer de les modifier.
+*/
+
+// Methode 1
+define('CAPITALE_SENEGAL', 'Dakar');
+/* On déclare la constante CAPITALE_SENEGAL et on lui affecte la valeur Dakar.
+Par convention on l'écrit en majuscules.
+*/
+echo CAPITALE_SENEGAL.'<br>';
+
+// Methode 2
+const CONSTANTE_PI = 3.14; // on peut aussi déclarer une constante avec "const"
+print CONSTANTE_PI."<br>";
+
+// --
+// Exercice: vous affichez "Bleu-Blanc-Rouge" en mettant le texte de chaque couleur dans une variable.
+
+// $bleu = "<div class=bu>Bleu</div>";
+// $blanc = "<div class=bc>Blanc</div>";
+// $rouge = "<div class=red>Rouge</div>";
+
+$vert = 'vert';
+$jaune = 'jaune';
+$rouge = 'rouge';
+
+echo "<span class='vert'>$vert</span> - <span class='jaune'>$jaune</span> - <span class='rouge'>$rouge</span> <br>";
+echo "$vert-$jaune-$rouge";
+
+// -------------------------------
+echo '<h2> Opérateurs arithmétiques  </h2>';
+// -------------------------------
+$a = 10;
+$b = 2;
+
+echo $a + $b .'<br>'; //affiche 12
+echo $a - $b .'<br>'; // affiche 8
+echo $a * $b .'<br>'; // affiche 20
+echo $a / $b .'<br>'; // affiche 5
+echo $a % $b .'<br>'; // affiche 0(vous avez 3 billes que vous répartissez sur 2 joueurs.Il reste 1 bille dans la main) : 3%2 = 1;
+
+// -------
+// Les opérateurs combinés :
+$a += $b; // équivant à $a = $a + $b soit $a vaut  12.
+$a -=$b; // équivaut à $a = $a -$b soit $a vaut 10 à la fin.
+$a *= $b; // équivaut à $a = $a *$b soit $a vaut 20 à la fin.
+$a /= $b; // équivaut à $a = $a / $b soit $a vaut 10 à la fin.
+$a %= $b; //  équivaut à $a = $a % $b soit $a vaut 0 à la fin.
+
+//--------------
+//incrémenter et décrementer 
+$i = 0;$i++; // incrementation : on ajoute 1 à $i. $i vaut donc 1.
+$i--; // décrementation : on retire 1 a $i. $i vaut donc 1.
